920e88710792c709e9f4c0a1da853e76daf89e98
Axel Pröger <a.proeger@gmail.com>
2024-04-28 19:57:45 +0200

    refactor

--- lib/logger.py
+++ lib/logger.py
@@ -27,24 +27,22 @@ def log(temperature, humidity, soil_moisture, light_level):
     }
     
     check_directories(year, month)
-    write_csv_file("data/"+year+"/"+month+"/csv/"+year+"-"+month+"-"+day+".csv", FIELDNAMES, data)
-    write_json_file("data/"+year+"/"+month+"/json/"+year+"-"+month+"-"+day+".json", data)
-    take_picture("data/"+year+"/"+month+"/images/"+year+"-"+month+"-"+day+"-"+hour+"-"+minute+"-"+second+".jpg")
+    write_csv_file(f"data/{year}/{month}/csv/{year}-{month}-{day}.csv", FIELDNAMES, data)
+    write_json_file(f"data/{year}/{month}/json/{year}-{month}-{day}.json", data)
+    take_picture(f"data/{year}/{month}/images/{year}-{month}-{day}-{hour}-{minute}-{second}.jpg")
 
 
 def check_directories(year, month):
 
-    if not os.path.isdir("data/" + year):
-        os.mkdir("data/" + year)
-
-    if not os.path.isdir("data/" + year + "/" + month):
-        os.mkdir("data/" + year + "/" + month)
+    base_path = f"data/{year}/{month}"
     
-    if not os.path.isdir("data/" + year + "/" + month + "/images"):
-        os.mkdir("data/" + year + "/" + month + "/images")
+    create_directory(base_path)
+    create_directory(f"{base_path}/images")
+    create_directory(f"{base_path}/csv")
+    create_directory(f"{base_path}/json")
 
-    if not os.path.isdir("data/" + year + "/" + month + "/csv"):
-        os.mkdir("data/" + year + "/" + month + "/csv")
 
-    if not os.path.isdir("data/" + year + "/" + month + "/json"):
-        os.mkdir("data/" + year + "/" + month + "/json")
+def create_directory(path):
+
+    if not os.path.isdir(path):
+        os.mkdir(path)
