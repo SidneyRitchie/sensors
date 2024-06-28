3eb1cc2406f09e3a1366d982c415bebb337efe47
SidneyRitchie <login@keno-andersen.de>
2024-06-28 17:53:32 +0200

    noobtry2

--- main.py
+++ main.py
@@ -7,32 +7,16 @@ from sensors.light import get_light_level
 
 from lib.logger import log
 
-try:
 	
-	while True:
 
-		light_level = get_light_level()
-		humidity = get_humidity()
-		temperature = get_temperature()
-		soil_moisture = get_soil_moisture()
+light_level = get_light_level()
+humidity = get_humidity()
+temperature = get_temperature()
+soil_moisture = get_soil_moisture()
 
-		if(not soil_moisture):
-			soil_moisture_text = "Trocken"
-		else:
-			soil_moisture_text = "Feucht"
-
-		print("-------------------------")
-		print("| {0} |".format(datetime.now().strftime("%Y.%m.%d - %H:%M:%S")))
-		print("-------------------------")
-		print("Temperatur = {0:0.1f}°C".format(temperature))
-		print("Luftfeuchtigkeit = {0:0.1f}%".format(humidity))
-		print("Lichtstärke = {0:.2f}lux".format(light_level))
-		print("Bodenfeuchtigkeit = {0}".format(soil_moisture_text))
-		print("")
-
-		log(temperature,humidity,soil_moisture,light_level)
-
-		sleep(30)
-
-except KeyboardInterrupt:
-	print("Script end!")
+if(not soil_moisture):
+	soil_moisture_text = "Trocken"
+else:
+	soil_moisture_text = "Feucht"
+	log(temperature,humidity,soil_moisture,light_level)
+	sleep(30)
