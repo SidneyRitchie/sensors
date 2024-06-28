44661d1cbe75502377c0e8c9c1882934a568de58
SidneyRitchie <login@keno-andersen.de>
2024-06-28 17:40:19 +0200

    remove print

--- main.py
+++ main.py
@@ -21,18 +21,8 @@ try:
 		else:
 			soil_moisture_text = "Feucht"
 
-		print("-------------------------")
-		print("| {0} |".format(datetime.now().strftime("%Y.%m.%d - %H:%M:%S")))
-		print("-------------------------")
-		print("Temperatur = {0:0.1f}°C".format(temperature))
-		print("Luftfeuchtigkeit = {0:0.1f}%".format(humidity))
-		print("Lichtstärke = {0:.2f}lux".format(light_level))
-		print("Bodenfeuchtigkeit = {0}".format(soil_moisture_text))
-		print("")
-
 		log(temperature,humidity,soil_moisture,light_level)
 
 		sleep(30)
 
-except KeyboardInterrupt:
-	print("Script end!")
+
